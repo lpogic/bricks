b9352242e29d4d411d46ec3295ba95a953514745
LPO <pomluk1996@gmail.com>
2021-03-25 11:05:53 +0100

    module info update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
-module lwjgl.main {
+open module lwjgl.main {
     requires org.joml;
     requires org.lwjgl.glfw;
     requires org.lwjgl.opengl;
     requires org.lwjgl.stb;
     requires brackettree;
     requires suite.main;
+    exports app.model.font;
+    exports app.model.graphic;
+    exports app.model.image;
+    exports app.model.input;
+    exports app.model.trade;
+    exports app.model.var;
+    exports app.model.wall;
 }
